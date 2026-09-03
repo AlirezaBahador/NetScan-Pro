866a40266c4842b1058a5974641b3e0afb7ee045
Alireza Bahador <168124787+AlirezaBahador@users.noreply.github.com>
2026-09-03 10:50:03 +0330

    Create __init__.py

---
+++ Netscan/__init__.py
@@ -0,0 +1,12 @@
+"""
+netscan-pro
+~~~~~~~~~~~
+A fast, asynchronous TCP/UDP network reconnaissance toolkit with
+service banner grabbing and lightweight OS fingerprinting.
+
+Intended strictly for use against hosts and networks you own or are
+explicitly authorized to test.
+"""
+
+__version__ = "1.2.0"
+__author__ = "Alireza"
